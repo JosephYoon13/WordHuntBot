a31c6f51c48735f45ec1962ab3ff7e4b5571ec00
JosephYoon13 <95374942+JosephYoon13@users.noreply.github.com>
2023-07-21 11:06:40 -0700

    moving directions test

--- main.py
+++ main.py
@@ -1,14 +1,78 @@
 from mouse.mouse_control import MouseControl
+import trie
 
-directions_list = [[(0, 1), (1, 0), (2, 0), (3, 0), (2, 1), (1, 1), (1, 2)],
-              [(0, 2), (0, 1), (1, 0), (1, 1), (2, 1), (3, 0), (3, 1)]
-              ]
+# directions_list = [[(0, 1), (1, 0), (2, 0), (3, 0), (2, 1), (1, 1), (1, 2)],
+#               [(0, 2), (0, 1), (1, 0), (1, 1), (2, 1), (3, 0), (3, 1)]
+#               ]
 
 # directions_list = [[(1, 2), (2, 1)],
 #               [(0, 2), (1, 1)]
 #               ]
 
 if __name__ == "__main__":
+    print("Input board: ")
+    text = input()
+
+    t = trie.Trie()
+    hugelist = []
+    with open("dictionary.txt", "r") as f:
+        hugelist = f.read().split()
+
+    for word in hugelist:
+        t.insert(word.lower())
+
+
+    found_words = {}
+    directions = []
+    visited = []
+    matrix = [[None for c in range(4)] for r in range(4)]
+
+    k = 0
+    for m in range(4):
+        for n in range(4):
+            matrix[m][n] = text[k]
+            k += 1
+
+    def recurse(i, j, dir, word):
+        if i < 0 or j < 0 or i > 3 or j > 3:
+            return
+        if not t.startsWith(word) or (i, j) in visited:
+            return
+
+        word += matrix[i][j]
+        visited.append((i, j))
+        directions.append(dir)
+
+        if len(word) >= 3 and t.search(word) and word not in found_words:
+            found_words[word] = list(directions)
+
+        recurse(i, j + 1, (i, j + 1), word)
+        recurse(i, j - 1, (i, j - 1), word)
+        recurse(i + 1, j, (i + 1, j), word)
+        recurse(i - 1, j, (i - 1, j), word)
+        recurse(i + 1, j + 1, (i + 1, j + 1), word)
+        recurse(i + 1, j - 1, (i + 1, j - 1), word)
+        recurse(i - 1, j + 1, (i - 1, j + 1), word)
+        recurse(i - 1, j - 1, (i - 1, j - 1), word)
+        del visited[-1]
+        del directions[-1]
+        word = word[:-1]
+
+    for i in range(4):
+        for j in range(4):
+            recurse(i, j, (i, j), '')
+
+    found_words = dict(
+        sorted(found_words.items(), key=lambda x: len(x[0]), reverse=True))
+
+    for w, d in found_words.items():
+        print(w, d)
+    print(len(found_words))
+   
+    directions_list = found_words.values()
+
+
+
     mouse = MouseControl()
     mouse.prepare()
     # directions = directions_list[0]
