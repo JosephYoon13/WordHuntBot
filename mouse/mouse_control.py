6361f7bd9610173dded0027b0439ca2f5a3653cb
JosephYoon13 <95374942+JosephYoon13@users.noreply.github.com>
2023-07-21 11:16:04 -0700

    moving directions test

--- mouse/mouse_control.py
+++ mouse/mouse_control.py
@@ -55,7 +55,7 @@ class MouseControl:
             dy -= 1
         self.x_curr = new_x
         self.y_curr = new_y
-        time.sleep(.2)
+        time.sleep(0)
 
     def move(self, new_x, new_y):
         dx = 12
@@ -75,7 +75,7 @@ class MouseControl:
         self.y_curr = new_y
         self.client.state = [int(self.pressed), dy, dx, 0]
         self.client.send_current()
-        time.sleep(.2)
+        time.sleep(0)
 
 
 
