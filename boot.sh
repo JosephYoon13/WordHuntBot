1d71b09d9848079fea1e8aff9c7d7ce3f628f22e
JosephYoon13 <95374942+JosephYoon13@users.noreply.github.com>
2023-07-20 19:56:36 -0700

    changed device name and boot commands

--- boot.sh
+++ boot.sh
@@ -5,16 +5,13 @@ sudo /etc/init.d/bluetooth start
 # Update  mac address
 ./updateMac.sh
 #Update Name
-./updateName.sh ThanhLe_Keyboard_Mouse
+./updateName.sh My_Mouse_Keyboard
 #Get current Path
 export C_PATH=$(pwd)
 
 tmux kill-window -t thanhle:app >/dev/null 2>&1
 
 [ ! -z "$(tmux has-session -t thanhle 2>&1)" ] && tmux new-session -s thanhle -n app -d
-[ ! -z "$(tmux has-session -t thanhle:app 2>&1)" ] && {
-    tmux new-window -t thanhle -n app
-}
 [ ! -z "$(tmux has-session -t thanhle:app.1 2>&1)" ] && tmux split-window -t thanhle:app -h
 [ ! -z "$(tmux has-session -t thanhle:app.2 2>&1)" ] && tmux split-window -t thanhle:app.1 -v
 tmux send-keys -t thanhle:app.0 'cd $C_PATH/server && sudo ./btk_server.py' C-m
