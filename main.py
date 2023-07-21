096b6b03232044be70c8745e42b609a7a8d52378
JosephYoon13 <95374942+JosephYoon13@users.noreply.github.com>
2023-07-21 09:20:55 -0700

    moving directions test

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ directions_list = [[(1, 0), (0, 1), (0, 0), (1, 1), (2, 1), (2, 2), (1, 2), (0, 
 if __name__ == "__main__":
     mouse = MouseControl()
     mouse.prepare()
-    directions = directions_list[0]
-    d = directions[0]
-    x, y = d
-    mouse.goto(x,y)
-    mouse.goto(0, 3)
+    # directions = directions_list[0]
+    # d = directions[0]
+    # x, y = d
+    # mouse.goto(x,y)
+    # mouse.goto(0, 3)
     
-    # for directions in directions_list:
-    #     init_x, init_y = directions[0]
-    #     mouse.goto(init_x, init_y)
-    #     mouse.press()
-    #     for d in directions:
-    #         x, y = d
-    #         mouse.goto(x, y)
-    #     mouse.press()
+    for directions in directions_list:
+        init_x, init_y = directions[0]
+        mouse.goto(init_x, init_y)
+        mouse.press()
+        for d in directions:
+            x, y = d
+            mouse.goto(x, y)
+        mouse.press()
