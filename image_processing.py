701875655533d889072286f7e0c61115afccb44f
JosephYoon13 <95374942+JosephYoon13@users.noreply.github.com>
2023-07-20 19:50:10 -0700

    reset commits

---
+++ image_processing.py
@@ -0,0 +1,44 @@
+import pytesseract as tess
+from PIL import Image
+import numpy as np
+
+
+def processImage():
+    im1 = Image.open('boardpic.jpg')
+    im1 = im1.convert('RGB')
+
+    width, height = im1.size
+    for x in range(width):
+        for y in range(height):
+            r, g, b = im1.getpixel((x, y))
+            gray = int(r * 299/1000 + g * 587/1000 + b * 114/1000)
+            im1.putpixel((x, y), (gray, gray, gray))
+
+    # im1.save('out.png')
+
+    # im = Image.open('out.png')
+    im1 = im1.convert('RGBA')
+    data = np.array(im1)
+    rgb = data[:, :, :3]
+    black = [0, 0, 0]
+    white = [255, 255, 255, 255]
+    mask = np.all(rgb == black, axis=-1)
+    data[np.logical_not(mask)] = white
+    new_im = Image.fromarray(data)
+    # new_im.save('black_image.png')
+
+    text = tess.image_to_string(new_im, config='--psm 6')
+    text = ''.join(text.splitlines())
+    text = text.replace(' ', '')
+    text = text.replace('|', 'I')
+    text = text.lower()
+
+    if len(text) < 16:
+        print("Invalid; enter board: ")
+        text = input()
+    print(text)
+
+    return text
+
+
+# processImage()
---
+++ test.py
@@ -0,0 +1,83 @@
+from collections import OrderedDict
+import trie
+from image_processing import processImage
+
+
+def main():
+
+    # text = processImage()
+    print("Input board: ")
+    text = input()
+
+    t = trie.Trie()
+    hugelist = []
+    with open("dictionary.txt", "r") as f:
+        hugelist = f.read().split()
+
+    for word in hugelist:
+        t.insert(word.lower())
+
+    # list = []
+    # word = ''
+    # def loop(node: trie.TrieNode, word: str):
+    #     if node.children:
+    #         for k, v in node.children.items():
+    #             loop(v, word + k)
+    #     else:
+    #         list.append(word)
+
+    # loop(cur, '')
+    # print(list)
+
+    found_words = {}
+    directions = []
+    visited = []
+    matrix = [[None for c in range(4)] for r in range(4)]
+
+    k = 0
+    for m in range(4):
+        for n in range(4):
+            matrix[m][n] = text[k]
+            k += 1
+
+    def recurse(i, j, dir, word):
+        if i < 0 or j < 0 or i > 3 or j > 3:
+            return
+        if not t.startsWith(word) or (i, j) in visited:
+            return
+
+        word += matrix[i][j]
+        visited.append((i, j))
+        directions.append(dir)
+
+        if len(word) >= 3 and t.search(word) and word not in found_words:
+            found_words[word] = list(directions)
+
+        recurse(i, j + 1, 'R', word)
+        recurse(i, j - 1, 'L', word)
+        recurse(i + 1, j, 'D', word)
+        recurse(i - 1, j, 'U', word)
+        recurse(i + 1, j + 1, 'DR', word)
+        recurse(i + 1, j - 1, 'DL', word)
+        recurse(i - 1, j + 1, 'UR', word)
+        recurse(i - 1, j - 1, 'UL', word)
+        del visited[-1]
+        del directions[-1]
+        word = word[:-1]
+
+    for i in range(4):
+        for j in range(4):
+            recurse(i, j, (i, j), '')
+
+    found_words = dict(
+        sorted(found_words.items(), key=lambda x: len(x[0]), reverse=True))
+
+    for w, d in found_words.items():
+        print(w, d)
+    print(len(found_words))
+    # if type(found_words['hia'][0]) is tuple:
+    #     print('yes')
+
+
+if __name__ == "__main__":
+    main()
