5e46c58293abaffd7a291eb10143f4d78c157796
JosephYoon13 <95374942+JosephYoon13@users.noreply.github.com>
2023-07-21 13:54:52 -0700

    moving directions test

--- mouse/mouse_control.py
+++ mouse/mouse_control.py
@@ -75,7 +75,7 @@ class MouseControl:
         self.y_curr = new_y
         self.client.state = [int(self.pressed), dy, dx, 0]
         self.client.send_current()
-        # time.sleep(.05)
+        time.sleep(.05)
 
     def moveReverse(self, new_x, new_y):
         dx = 12
