034c342bd9609e3b80df10befa30eeea6c87b26d
JosephYoon13 <95374942+JosephYoon13@users.noreply.github.com>
2023-07-23 10:44:10 -0700

    boot testing

--- boot.sh
+++ boot.sh
@@ -21,4 +21,4 @@ tmux kill-window -t thanhle:app >/dev/null 2>&1
 tmux send-keys -t thanhle:app.0 'cd $C_PATH/server && sudo python3 ./btk_server.py' C-m
 tmux send-keys -t thanhle:app.1 'cd $C_PATH/mouse  && reset' C-m
 tmux send-keys -t thanhle:app.2 'cd $C_PATH/keyboard  && reset' C-m
-tmux send-keys -t thanhle:app.1 'bluetoothctl && agent on && default-agent && discoverable on' C-m
+tmux send-keys -t thanhle:app.1 'bluetoothctl && exit' C-m
