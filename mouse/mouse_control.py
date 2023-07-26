2f1bf6ad01133488ee8975aa4acf698e44500f23
JosephYoon13 <95374942+JosephYoon13@users.noreply.github.com>
2023-07-26 13:55:31 -0700

    delay tests

--- mouse/mouse_control.py
+++ mouse/mouse_control.py
@@ -57,7 +57,7 @@ class MouseControl:
             dy -= 1
         self.x_curr = new_x
         self.y_curr = new_y
-        # time.sleep(.05)
+        time.sleep(.05)
 
     def move(self, new_x, new_y):
         dx = 12
